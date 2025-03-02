a8317bb5ba024a18bed51f88a4100ee741eddf20
CriisMi <87492995+CriisMi@users.noreply.github.com>
2025-03-02 21:50:21 +0200

    zz test secure cookie

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env("URL", "https://test-admin.zorzon.com.ro"), // Set correct public URL
+  proxy: env.bool("STRAPI_PROXY", false),
   app: {
     keys: env.array("APP_KEYS"),
   },
