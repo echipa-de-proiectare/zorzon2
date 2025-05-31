5c3f94ae2eaa84a92aa8013d1277e83931920e81
CriisMi <87492995+CriisMi@users.noreply.github.com>
2025-05-31 18:24:08 +0300

    caddy reverse proxy ar viewer

--- frontend/src/user/components/ArViewer.jsx
+++ frontend/src/user/components/ArViewer.jsx
@@ -1,16 +1,15 @@
 import { useContext } from "react";
 import { UserContext } from "../utility/UserContext";
-
+const VITE_AR_VIEWER_URL = import.meta.env.VITE_AR_VIEWER_URL; // Access the environment variable
 const ArViewer = () => {
   const context = useContext(UserContext);
   const jwt = context.jwt || sessionStorage.getItem("jwt");
-  console.log("JWT in ArViewer:", jwt);
 
   return (
     <iframe
       width="100%"
       height="100%"
-      src={`http://localhost:5174/?token=${encodeURIComponent(jwt)}`}
+      src={`${VITE_AR_VIEWER_URL}/?token=${encodeURIComponent(jwt)}`}
       allow="camera; gyroscope; accelerometer; xr-spatial-tracking"
     ></iframe>
   );
